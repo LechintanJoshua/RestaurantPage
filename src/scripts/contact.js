62dd33e01a4f43de7e032637870c68b4093fcaf7
LechintanJoshua <jlechintan79@gmail.com>
2026-04-27 12:13:32 +0300

    Create Reservation card
    Restructure loop to reduce reapeating code

--- src/scripts/contact.js
+++ src/scripts/contact.js
@@ -17,6 +17,16 @@ export default class Contact {
         return div;
     }
 
+    #addListElements (ul, infoList) {
+        for (let i = 0; i < 3; i++) {
+            const li = document.createElement('li');
+
+            li.textContent = infoList[i];
+
+            ul.appendChild(li);
+        }
+    }
+
     #createChefCard() {
         const div = document.createElement('div');
         const p = document.createElement('p');
@@ -27,15 +37,29 @@ export default class Contact {
             'alexandru@fourtimtrees.ro',
         ];
 
-        for (let i = 0; i < 3; i++) {
-            const li = document.createElement('li');
-
-            li.textContent = info[i];
-
-            ul.appendChild(li);
-        }
+        this.#addListElements(ul, info);
 
         p.textContent = 'Alexandru Mureșan';
+
+        div.appendChild(p);
+        div.appendChild(ul);
+        div.classList.add('card');
+
+        return div;
+    }
+
+    #createReservationCard() {
+        const div = document.createElement('div');
+        const p = document.createElement('p');
+        const ul = document.createElement('ul');
+        const info = ['Head Chef & Co-Fondator',
+            '+40 722 111 222',
+            'alexandru@fourtimtrees.ro',
+        ];
+
+        this.#addListElements(ul, info);
+
+        p.textContent = 'Elena Dumitrescu';
 
         div.appendChild(p);
         div.appendChild(ul);
