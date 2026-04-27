e3090f674d7377ac6946a045e9e92fe7df43f878
LechintanJoshua <jlechintan79@gmail.com>
2026-04-27 07:08:45 +0300

    Create and attach chef details card

--- src/scripts/contact.js
+++ src/scripts/contact.js
@@ -17,7 +17,35 @@ export default class Contact {
         return div;
     }
 
+    #createChefCard() {
+        const div = document.createElement('div');
+        const p = document.createElement('p');
+        const ul = document.createElement('ul');
+
+        const info = ['Head Chef & Co-Fondator',
+            '+40 722 111 222',
+            'alexandru@fourtimtrees.ro',
+        ];
+
+        for (let i = 0; i < 3; i++) {
+            const li = document.createElement('li');
+
+            li.textContent = info[i];
+
+            ul.appendChild(li);
+        }
+
+        p.textContent = 'Alexandru Mureșan';
+
+        div.appendChild(p);
+        div.appendChild(ul);
+        div.classList.add('card');
+
+        return div;
+    }
+
     createContact() {
         this.#content.appendChild(this.#createTitleCard());
+        this.#content.appendChild(this.#createChefCard());
     }
 }
