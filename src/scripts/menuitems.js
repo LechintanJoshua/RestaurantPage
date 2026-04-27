66d7201c9723f9d80e008a404f5a70249b9e883e
LechintanJoshua <jlechintan79@gmail.com>
2026-04-27 15:39:46 +0300

    Finish MenuItem class

--- src/scripts/menuitems.js
+++ src/scripts/menuitems.js
@@ -13,4 +13,54 @@ export default class MenuItem {
         this.#quantity = quantity;
         this.#item.classList.add('card');
     }
+
+    #createTitle() {
+        const p = document.createElement('p');
+
+        p.textContent = this.#title;
+
+        return p;
+    }
+
+    #createFoodDescription() {
+        const p = document.createElement('p');
+
+        p.textContent = this.#description;
+
+        return p;
+    }
+
+    #createPrice() {
+        const p = document.createElement('p');
+
+        p.textContent = this.#price;
+
+        return p;
+    }
+
+    #createQuanity() {
+        const p = document.createElement('p');
+
+        p.textContent = this.#quantity;
+
+        return p;
+    }
+
+    #createFoodInfo () {
+        const div = document.createElement('div');
+
+        div.classList.add('details');
+        div.appendChild(this.#createPrice());
+        div.appendChild(this.#createQuanity());
+
+        return div;
+    }
+
+    createMenuItem() {
+        this.#item.appendChild(this.#createTitle());
+        this.#item.appendChild(this.#createFoodDescription());
+        this.#item.appendChild(this.#createFoodInfo());
+
+        return this.#item;
+    }
 }
